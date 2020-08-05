e2358f9de6351a71812553a49d04f64975a1bc52
Stav Yaar Bar <anistu@gmail.com>
2020-08-05 18:52:37 +0300

    improved bg img

---
+++ js/user-prefs-controller.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function showAge(newVal) {
+    document.getElementById("sAge").innerHTML = newVal;
+}
+
+
+
+function onSubmitPrefs(event){
+    event.preventDefault();
+    console.log('hsdsdsdsdsdsey');
+}
