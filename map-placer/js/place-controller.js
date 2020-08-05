8efccd273688ca47aad11c94ce0a29247a56d9d1
Stav Yaar Bar <anistu@gmail.com>
2020-08-05 22:07:54 +0300

    a good thing finally

--- map-placer/js/place-controller.js
+++ map-placer/js/place-controller.js
@@ -1,38 +1,65 @@
 'use strict'
 
-// let gMap;
-
-// function initMap(lat=29.5577, lng=34.9519) {
-//     //            if (!lat) lat = 32.0749831;
-//     //            if (!lng) lat = 34.9120554;
-//     const elMap = document.querySelector('#map');
-//     var options = {
-//         center: { lat, lng },
-//         zoom: 10
-//     };
-
-//     gMap = new google.maps.Map(
-//         elMap,
-//         options
-//     );
-
-//     var marker = new google.maps.Marker({
-//         position: { lat, lng },
-//         map: gMap,
-//         title: 'Hello World!'
-//     });
-// }
-
-// initMap(29.5577, 34.9519)
+let gMap;
 
 
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    var uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
+function initMap(lat = 29.5577, lng = 34.9519) {
+    //            if (!lat) lat = 32.0749831;
+    //            if (!lng) lat = 34.9120554;
+    var elMap = document.querySelector('#map');
+    var options = {
+        center: { lat, lng },
+        zoom: 8
+    };
+
     var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 4, center: uluru });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({ position: uluru, map: map });
+        elMap,
+        options
+    );
+
+    var marker = new google.maps.Marker({
+        position: { lat, lng },
+        map,
+        title: 'Hello World!'
+    });
+
+
+    map.addListener('click', function(e) {
+        console.log(e.latLng.lng());
+        placeMarkerAndPanTo(e.latLng, map);
+      });
+
+
 }
+
+function placeMarkerAndPanTo(latLng, map) {
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+    });
+    // map.panTo(latLng);
+}
+
+function getPosition() {
+    if (!navigator.geolocation) {
+        alert("HTML5 Geolocation is not supported in your browser.");
+        return;
+    }
+
+    // One shot position getting or continus watch
+    return navigator.geolocation.getCurrentPosition(showLocation, handleLocationError);
+    // navigator.geolocation.watchPosition(showLocation, handleLocationError);
+}
+
+function showLocation(position) {
+    console.log(position);
+
+
+    initMap(position.coords.latitude, position.coords.longitude);
+}
+
+function handleLocationError(error) {
+    return
+}
+
+
